e7252063b3909308a674871c5458bc826831f25e
Chris Birchall <c-birchall@m3.com>
2014-09-24 15:11:21 +0900

    Add config options to customize SpyMemcached's internal queues
    Default arguments are provided, and the default behavior is to use Spy's
    default queue implementations, so backwards compatibility is preserved.

--- src/main/scala/shade/memcached/MemcachedImpl.scala
+++ src/main/scala/shade/memcached/MemcachedImpl.scala
@@ -271,6 +271,9 @@ class MemcachedImpl(config: Configuration, ec: ExecutionContext) extends Memcach
           case FailureMode.Redistribute =>
             SpyFailureMode.Redistribute
         })
+        .setOpQueueFactory(config.opQueueFactory.orNull)
+        .setReadOpQueueFactory(config.readQueueFactory.orNull)
+        .setWriteOpQueueFactory(config.writeQueueFactory.orNull)
 
       val withTimeout = config.operationTimeout match {
         case duration: FiniteDuration =>
